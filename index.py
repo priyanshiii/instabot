84888083071c5098a410a63e14fddb84c4ef0ba1
priyanshiii <priyanshisharma.cp@gmail.com>
2017-07-05 21:34:18 +0530

    instagram

--- index.py
+++ index.py
@@ -1,16 +1,20 @@
-import requests,urllib
-
+import requests
+import urllib
+#accesstoken generated
 ACCESS_TOKEN = "1655525547.edb4001.256a7090fa374b04972e0d76eecd1849"
 
+#url of instagram api
 BASE_URL = "https://api.instagram.com/v1/"
 
-
+#function to define "self"
 def my_info():
 
     request_url = BASE_URL + 'users/self/?access_token=%s' % ACCESS_TOKEN
     print 'GET request url : %s' % request_url
     my_info = requests.get(request_url).json()
 
+#to check the status code if it is okay
+    #meta code means data or information about code
     if my_info['meta']['code'] == 200:
         if len(my_info['data']):
             print 'Username: %s' % (my_info['data']['username'])
@@ -18,11 +22,12 @@ def my_info():
             print 'No. of people you are following: %s' % (my_info['data']['counts']['follows'])
             print 'No. of posts: %s' % (my_info['data']['counts']['media'])
         else:
-            print 'User does not exist!'
+            print 'errrr,User does not exist!'
     else:
         print 'Status code other than 200 received'
 my_info()
 
+# code to retrieve friend id through access token
 
 def get_friend_id(insta_user):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_user, ACCESS_TOKEN)
@@ -36,6 +41,7 @@ def get_friend_id(insta_user):
     else:
         print 'Network Problem'
 
+#code to retrieve friend information(followers,following,media,username)
 
 def get_friend_info(insta_user):
     friend_id = get_friend_id(insta_user)
@@ -56,9 +62,12 @@ def get_friend_info(insta_user):
     else:
         print 'Status code other than 200 received!'
 
-insta_user = raw_input('enter the user name')
 
+
+insta_user = raw_input('enter the user name')
 get_friend_info(insta_user)
+#to see your own media(posts,profile picture)
+#urrlib is a library used for storing the id(user id) in jpeg format
 def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -69,9 +78,79 @@ def get_own_post():
             image_name = own_media['data'][0]['id'] + '.jpeg'
             image_url = own_media['data'][0]['images']['standard_resolution']['url']
             urllib.urlretrieve(image_url, image_name)
-            print 'Your image has been downloaded!'
+            print 'image downloaded!'
         else:
-            print 'Post does not exist!'
+            print 'errr Post does not exist!'
+    else:
+        print 'Status code other than 200!'
+
+#standard resolution is a kind of picture view,we can use thumbnail as well.
+
+#to get your own post(media)
+def image_url():
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            age_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'your image has been downloaded!'
+        else:
+            print 'errrr,Post does not exist!'
     else:
         print 'Status code other than 200 received!'
 
+
+def get_user_post(insta_username):
+    user_id = get_friend_id()(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'errr,Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details\n"
+        print "b.Get details of a user by username\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
+
+        print "j.Exit"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+            my_info()
+        elif choice == "b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_friend_info()(insta_username)
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+        elif choice == "j":
+            exit()
+        else:
+            print "wrong choice"
+            start_bot()
