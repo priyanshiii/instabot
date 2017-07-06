874cd2122d91d1819d76f2cad0a7de3748c8cdb0
priyanshiii <priyanshisharma.cp@gmail.com>
2017-07-06 17:23:18 +0530

    instagram

--- index.py
+++ index.py
@@ -25,17 +25,16 @@ def my_info():
             print 'errrr,User does not exist!'
     else:
         print 'Status code other than 200 received'
-my_info()
 
 # code to retrieve friend id through access token
 
-def get_friend_id(insta_user):
+def get_buddy_id(insta_user):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_user, ACCESS_TOKEN)
     print 'GET request url : %s' % request_url
-    friend_info = requests.get(request_url).json()
-    if friend_info['meta']['code'] == 200:
-        if len(friend_info['data']):
-            return friend_info['data'][0]['id']
+    buddy_info = requests.get(request_url).json()
+    if buddy_info['meta']['code'] == 200:
+        if len(buddy_info['data']):
+            return buddy_info['data'][0]['id']
         else:
             return None
     else:
@@ -43,20 +42,20 @@ def get_friend_id(insta_user):
 
 #code to retrieve friend information(followers,following,media,username)
 
-def get_friend_info(insta_user):
-    friend_id = get_friend_id(insta_user)
-    if friend_id == None:
+def get_buddy_info(insta_user):
+    buddy_id = get_buddy_id(insta_user)
+    if buddy_id == None:
         print 'User does not exist!'
-    request_url = (BASE_URL + 'users/%s?access_token=%s') % (friend_id, ACCESS_TOKEN)
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (buddy_id, ACCESS_TOKEN)
     print 'GET request url : %s' % request_url
-    friend_info = requests.get(request_url).json()
+    buddy_info = requests.get(request_url).json()
 
-    if friend_info['meta']['code'] == 200:
-        if len(friend_info['data']):
-            print 'Username: %s' % (friend_info['data']['username'])
-            print 'No. of followers: %s' % (friend_info['data']['counts']['followed_by'])
-            print 'No. of people you are following: %s' % (friend_info['data']['counts']['follows'])
-            print 'No. of posts: %s' % (friend_info['data']['counts']['media'])
+    if buddy_info['meta']['code'] == 200:
+        if len(buddy_info['data']):
+            print 'Username: %s' % (buddy_info['data']['username'])
+            print 'No. of followers: %s' % (buddy_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (buddy_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (buddy_info['data']['counts']['media'])
         else:
             print 'There is no data for this user!'
     else:
@@ -64,10 +63,10 @@ def get_friend_info(insta_user):
 
 
 
-insta_user = raw_input('enter the user name')
-get_friend_info(insta_user)
+
 #to see your own media(posts,profile picture)
 #urrlib is a library used for storing the id(user id) in jpeg format
+
 def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -75,28 +74,9 @@ def get_own_post():
 
     if own_media['meta']['code'] == 200:
         if len(own_media['data']):
+
             image_name = own_media['data'][0]['id'] + '.jpeg'
             image_url = own_media['data'][0]['images']['standard_resolution']['url']
-            urllib.urlretrieve(image_url, image_name)
-            print 'image downloaded!'
-        else:
-            print 'errr Post does not exist!'
-    else:
-        print 'Status code other than 200!'
-
-#standard resolution is a kind of picture view,we can use thumbnail as well.
-
-#to get your own post(media)
-def image_url():
-def get_own_post():
-    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (ACCESS_TOKEN)
-    print 'GET request url : %s' % (request_url)
-    own_media = requests.get(request_url).json()
-
-    if own_media['meta']['code'] == 200:
-        if len(own_media['data']):
-            image_name = own_media['data'][0]['id'] + '.jpeg'
-            age_url = own_media['data'][0]['images']['standard_resolution']['url']
             urllib.urlretrieve(image_url, image_name)
             print 'your image has been downloaded!'
         else:
@@ -105,11 +85,12 @@ def get_own_post():
         print 'Status code other than 200 received!'
 
 
-def get_user_post(insta_username):
-    user_id = get_friend_id()(insta_username)
+def get_user_post(insta_user):
+    user_id = get_buddy_id(insta_user)
     if user_id == None:
         print 'User does not exist!'
-        exit()
+
+
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_media = requests.get(request_url).json()
@@ -126,31 +107,45 @@ def get_user_post(insta_username):
         print 'Status code other than 200 received!'
 
 
+
+
+def like_post(insta_username):
+    media_id = get_user_post(insta_username)
+    request_url = (BASE_URL + 'media/%s/likes') % (media_id)
+    payload = {"access_token":ACCESS_TOKEN}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
+    if post_a_like['meta']['code'] == 200:
+        print 'Like successful!'
+    else:
+        print 'Like was unsuccessful. Try again!'
+
+
+
 def start_bot():
     while True:
         print '\n'
-        print 'Hey! Welcome to instaBot!'
-        print 'Here are your menu options:'
+        print 'choose from the following options'
         print "a.Get your own details\n"
         print "b.Get details of a user by username\n"
         print "c.Get your own recent post\n"
         print "d.Get the recent post of a user by username\n"
 
-        print "j.Exit"
+        print "e.Exit"
 
         choice = raw_input("Enter you choice: ")
         if choice == "a":
             my_info()
         elif choice == "b":
             insta_username = raw_input("Enter the username of the user: ")
-            get_friend_info()(insta_username)
+            get_buddy_info()(insta_username)
         elif choice == "c":
             get_own_post()
         elif choice == "d":
             insta_username = raw_input("Enter the username of the user: ")
             get_user_post(insta_username)
-        elif choice == "j":
+        elif choice == "e":
             exit()
         else:
             print "wrong choice"
-            start_bot()
+start_bot()
